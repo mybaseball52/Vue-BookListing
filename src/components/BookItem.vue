8c2b54ba530390ef1236b103378cea958c9f33c7
Kevin <mybaseball52@gmail.com>
2019-01-01 00:35:25 +0800

    add item page

---
+++ src/components/BookItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <li>{{book.title}}:{{book.author}}</li>
+</template>
+
+<script>
+export default {
+  name: 'BookItem',
+  data() {
+    return {
+      props: 'book',
+    };
+  },
+};
+</script>
+
+<style>
+</style>
